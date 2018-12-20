e8e59acf94329ecb59072d081336c7c059a898c5
morzhanov <vlad.morzhanov@gmail.com>
2018-12-20 14:45:08 +0200

    removed .env added .env example

--- backend/src/payment/providers/braintree.service.ts
+++ backend/src/payment/providers/braintree.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import * as Braintree from 'braintree';
 
 @Injectable()
 export class BraintreeService {
-  public getBraintreeInstance() {
+private connection;
+
+constructor(){
+  this.connection = Braintree.connect({
+    environment: brainTreeEnv,
+    merchantId: process.env.MERCHANT_ID,
+    publicKey: process.env.PUBLIC_KEY,
+    privateKey: process.env.PRIVATE_KEY,
+});
+}
+
+  public checkout(data) {
     return null;
   }
 }
