9a206b88d7e2c98ad2a1e12f6793e13c4ab1da40
morzhanov <vlad.morzhanov@gmail.com>
2018-12-20 17:23:21 +0200

    added base stripe config

--- backend/src/payment/providers/paypal.service.ts
+++ backend/src/payment/providers/paypal.service.ts
@@ -1,8 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import * as Stripe from 'stripe';
+import { STRIPE_API_KEY } from '../../common/constants';
 
 @Injectable()
 export class PayPalService {
-  public getPayPalInstance() {
-    return null;
+  private stripe;
+
+  constructor() {
+    this.stripe = new Stripe(STRIPE_API_KEY);
+  }
+
+  public async checkout() {
+    this.stripe.customers
+      .create({
+        email: 'foo-customer@example.com'
+      })
+      .then(customer => {
+        return this.stripe.customers.createSource(customer.id, {
+          source: 'tok_visa'
+        });
+      })
+      .then(source => {
+        return this.stripe.charges.create({
+          amount: 1600,
+          currency: 'usd',
+          customer: source.customer
+        });
+      })
+      .then(charge => {
+        // New charge created on a new customer
+      })
+      .catch(err => {
+        // Deal with an error
+      });
   }
 }
